27f9827e106eb5bafd77324903ca01df6d133623
Rajamani Neelamegam <nbrmani@gmail.com>
2023-05-08 17:26:29 -0700

    commit 2

--- LWC2/force-app/main/default/lwc/ltngRecordForm/ltngRecordForm.js
+++ LWC2/force-app/main/default/lwc/ltngRecordForm/ltngRecordForm.js
@@ -1,8 +1,6 @@
 import { LightningElement, api } from 'lwc';
 
 export default class LtngRecordForm extends LightningElement {
-
-    @api recordId:
+    @api recordId;
     @api objectApiName;
-
 }
